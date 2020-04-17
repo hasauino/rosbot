6852ab97a0c184cc7c36897c6a09700c9257d321
hasauino <oh91@windowslive.com>
2020-04-18 01:54:36 +0200

    update

--- motor_control/reading_encoder/reading_encoder.ino
+++ motor_control/reading_encoder/reading_encoder.ino
@@ -1,17 +1,28 @@
 #include "Motor.h"
 #include "helpers.h"
+#include <AutoPID.h>
+
+float data[1];
+double speed = 0.0;
+double setPoint = 20.0;
+double output = 0.0;
+AutoPID myPID(&speed, &setPoint, &output, -100, 100, 1.0, 10.0, 0.0);
+
 
 Motor motor;
-Plotter plot(100,2);
+Plotter plot(100);
+
+
 
 void setup() {
 plot.begin(115200);  
 motor.init(10);
-motor.set_power(100);
+myPID.setTimeStep(1);
 }
 
 void loop() {
-plot.data[0] = 100.0;
-plot.data[1] = rad2rpm(motor.get_speed());
-plot.update();
+motor.set_power(200);
+
+data[0] = motor.get_speed();
+plot.update(data, 1);
 }
