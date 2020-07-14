3d9cba74080c225c1b6a686d8e27031dd1d17033
hasauino <oh91@windowslive.com>
2020-07-15 01:37:34 +0200

    added Receiver class

--- communication.h
+++ communication.h
@@ -6,18 +6,91 @@
 #ifndef COMMUNICATION_H
 #define COMMUNICATION_H
 
-#define MSG_FIRST_BYTE 123 // first byte in received message
-#define MSG_SECOND_BYTE 55 // second byte in received message
+#define MSG_FIRST_BYTE 123 // first byte in received message (0x7B)
+#define MSG_SECOND_BYTE 55 // second byte in received message (0x37)
+#define MSG_BUFFER_LENGTH 50
 
 /* commands */
 #define SET_VELOCITY 'V' // message type is command
 
-/*
-  class Receiver() {
+class Receiver {
+
+
   private:
+    /*
+      location1 and location 2 will both be used interchangably to store new bytes being buffered
+      and the last message.
+      "buffer" points to one of these array locations, once the received sequence of bytes form a complete message,
+      it will switch to point to the other location, where the original location will be pointed at by the "lates_message".
+      So they swap everytime after a complete messgae is received.
+    */
+    byte location1[MSG_BUFFER_LENGTH];
+    byte location2[MSG_BUFFER_LENGTH];
+    byte* buffer = location1;
+    byte* latest_message = location2;
+    int index = 0;
+    bool is_there_new_msg = false;
+    // temporary variables (ex: lpop counters ...etc)
+    int i = 0;
+
 
   public:
-  Receiver(){}
-  }
-*/
+    Receiver() {}
+
+    void update(int received_byte) {
+
+      if (index > 1) {
+        buffer[index++] = received_byte;
+      }
+
+
+      else if (index == 1) {
+        if (received_byte == MSG_SECOND_BYTE) {
+          buffer[index++] = MSG_SECOND_BYTE;
+        }
+        else {
+          index = 0;
+          buffer[0] = 0; // wrong msg, reset first byte
+        }
+      }
+
+
+      else if (index == 0) {
+        if (received_byte == MSG_FIRST_BYTE) {
+          buffer[index++] = MSG_FIRST_BYTE;
+        }
+      }
+
+      if (index > 2) {
+        if (index > buffer[2] + 2) {
+          // if checksum is correct --- TODO
+          if (true) {
+            is_there_new_msg = true;
+            if (buffer == location1) {
+              buffer = location2;
+              latest_message = location1;
+            } else {
+              buffer = location1;
+              latest_message = location2;
+            }
+
+          }
+
+          index = 0; // end of msg, reset index
+          buffer[0] = 0; // end of msg, reset first byte
+          buffer[1] = 0; // reset second byte
+        }
+      }
+    }//update_buffer
+
+    byte* latest_msg() {
+      is_there_new_msg = false;
+      return latest_message;
+    }
+
+    bool available() {
+      return is_there_new_msg;
+    }
+};
+
 #endif
--- rosbot.ino
+++ rosbot.ino
@@ -2,9 +2,9 @@
 #include "communication.h"
 
 Robot robot;
+Receiver receiver;
 float v = 0, w = 0;
-byte message[50];
-int index = 0;
+byte* msg;
 
 void setup() {
   Serial.begin(115200);
@@ -12,41 +12,16 @@ void setup() {
 }
 
 void loop() {
-  robot.set_speed(v, w);
+  //robot.set_speed(v, w);
+  if (receiver.available()) {
+    msg = receiver.latest_msg();
+    for (int i = 0; i < 10; i++) {
+      Serial.write(msg[i]);
+    }
+  }
 }
 
 
 void serialEvent() {
-  if (index > 1) {
-    message[index++] = Serial.read();
-  }
-  else if (index == 1) {
-    if (Serial.read() == MSG_SECOND_BYTE) {
-      message[index++] = MSG_SECOND_BYTE;
-      Serial.write(1);
-    }
-    else {
-      index = 0;
-      message[0] = 0; // wrong msg, reset first byte
-    }
-  }
-  else if (index == 0) {
-    if (Serial.read() == MSG_FIRST_BYTE) {
-      message[index++] = MSG_FIRST_BYTE;
-      Serial.write(0);
-    }
-  }
-
-  if (index > 2) {
-    if (index > message[2] + 2) {
-      for (int i=0; i<index;i++){
-        Serial.write(message[i]);
-        }
-      index = 0; // end of msg, reset index
-      message[0] = 0; // end of msg, reset first byte
-      message[1] = 0; // reset second byte
-    }
-  }
-
-
-}//serialEvent
+  receiver.update(Serial.read());
+}
